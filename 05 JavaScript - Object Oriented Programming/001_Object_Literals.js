fc01e117b58f8d172a4679a27b7f81f7baaf26ed
y-developer <yasithanadeeshan@gmail.com>
2021-12-09 11:14:32 +0530

    lesson object literals

---
+++ 05 JavaScript - Object Oriented Programming/001_Object_Literals.js
@@ -0,0 +1,10 @@
+var person = {
+    personName : "Yasitha",
+    birthDay : function(){
+        return "Happy Birthday";
+    }
+}
+
+console.log(person); // object
+console.log(person.personName); // personName property inside the person object
+console.log(person.birthDay()); // birthDay() function inside the person object
