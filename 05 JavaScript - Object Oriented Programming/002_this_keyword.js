1f816d2d7915a7032801ccb18c58a2e4b0a60399
y-developer <yasithanadeeshan@gmail.com>
2021-12-09 11:06:32 +0530

    add this keyword lesson

---
+++ 05 JavaScript - Object Oriented Programming/002_this_keyword.js
@@ -0,0 +1,20 @@
+var person = {
+    personName : "Yasitha",
+    birthDay : function(){
+        return `Happy Birthday ${this.personName}!!!`; // use "this" keyword
+    }
+}
+
+console.log("")
+console.log(person.birthDay()); // call birthDay method inside the person object
+
+
+
+console.log("")
+console.log("==============================================================")
+console.log("")
+
+
+console.log(this) // this is a empty object does not have any properties and methods
+this.x = "this is a value of a propety in globle object" // use this as globle object
+console.log(this);
