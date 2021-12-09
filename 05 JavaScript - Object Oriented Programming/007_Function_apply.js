ea1150fb6913b89f2d9355125b9e9411e9264c07
y-developer <yasithanadeeshan@gmail.com>
2021-12-09 15:41:02 +0530

    function apply

---
+++ 05 JavaScript - Object Oriented Programming/007_Function_apply.js
@@ -0,0 +1,13 @@
+const student = {
+    firstName: "Smith",
+    section: "A"
+}
+
+let calculateTotalMarks = function (subject1, subject2, subject3) {
+    let totalMarks = subject1 + subject2 + subject3;
+    let message = `Hey ${this.firstName}, your total marks is : ${totalMarks}`;
+    console.log(message);
+}
+
+calculateTotalMarks.call(student, 60, 70, 80); // <-------------- call
+calculateTotalMarks.apply(student, [60, 70, 80]); // <---------------- apply - use array for apply
