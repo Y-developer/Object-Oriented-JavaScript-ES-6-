49efecadb57b7ff26cfff568b0a0d27e393b8423
y-developer <yasithanadeeshan@gmail.com>
2021-12-11 22:32:40 +0530

    Inheritance - __proto__

---
+++ 05 JavaScript - Object Oriented Programming/013_Inheritance-__proto__.js
@@ -0,0 +1,20 @@
+const person = {
+    firstName: "Smith",
+    lastName: "John",
+    age: 15,
+};
+
+const student = {
+    indexNumber : "136954R",
+    grade : 11,
+    isPassLastExam : function (){
+        return `yes, ${this.firstName} ${this.lastName} passed last exam`;
+    }
+};
+
+student.__proto__ = person;
+
+console.log(`Full Name : ${student.firstName} ${student.lastName}`);
+console.log(`Age : ${student.age}`);
+console.log(`Index Number : ${student.indexNumber}`);
+console.log(`Is last exam passed?  ${student.isPassLastExam()}`);
