0f7bdcffe0ce497614b6b564534b4fa667a90b4e
y-developer <yasithanadeeshan@gmail.com>
2021-12-12 23:18:16 +0530

    017 Closures and Encapsulation

---
+++ 05 JavaScript - Object Oriented Programming/017_Closures_and_Encapsulation.js
@@ -0,0 +1,33 @@
+function bankAccount(){
+    var accontBalance = 1000;
+    
+    function deposit(depositAmount){
+        accontBalance += depositAmount;
+    };
+
+    function withdrow(withdrowAmount){
+        accontBalance -= withdrowAmount;
+    };
+
+    function getAccountBalance(){
+        return accontBalance;
+    }
+
+    return {deposit, withdrow, getAccountBalance};
+}
+
+const firstAccount = bankAccount();
+console.log(`First account initial balance Rs. ${firstAccount.getAccountBalance()}`)
+firstAccount.deposit(200);
+console.log(`First account balance after deposit Rs. ${firstAccount.getAccountBalance()}`);
+firstAccount.withdrow(50);
+console.log(`First account balance after withdrow Rs. ${firstAccount.getAccountBalance()}`);
+
+console.log("");
+
+const secoundAccount = bankAccount();
+console.log(`Secound account initial balance Rs. ${secoundAccount.getAccountBalance()}`)
+secoundAccount.deposit(500);
+console.log(`Secound account balance after deposit Rs. ${secoundAccount.getAccountBalance()}`);
+secoundAccount.withdrow(200);
+console.log(`Secound account balance after withdrow Rs. ${secoundAccount.getAccountBalance()}`);
