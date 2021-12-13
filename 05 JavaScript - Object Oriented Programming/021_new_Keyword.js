44116d30c8d7c8260c12afd81dc14c21ca24156a
y-developer <yasithanadeeshan@gmail.com>
2021-12-13 13:34:15 +0530

    update camel case to pascal case

--- 05 JavaScript - Object Oriented Programming/021_new_Keyword.js
+++ 05 JavaScript - Object Oriented Programming/021_new_Keyword.js
@@ -1,4 +1,4 @@
-function employee(paraName,paraId,paraSalary){
+function Employee(paraName,paraId,paraSalary){
     // initialize propeties & methods using this keyword
     this.name = paraName;
     this.id = paraId;
@@ -9,8 +9,8 @@ function employee(paraName,paraId,paraSalary){
     }
 }
 
-employee1 = new employee("Smith", "2324X", 2600);
-employee2 = new employee("John", "5364X", 3600);
+employee1 = new Employee("Smith", "2324X", 2600);
+employee2 = new Employee("John", "5364X", 3600);
 
 
 console.log(employee1);
